b0b0c3272adeb0c5815bef45fc3c65b8b81f5923
Abin John <abin.john84@gmail.com>
2025-02-06 09:00:53 -0500

    Details as Table

--- src/components/DeploymentDetailsPage.jsx
+++ src/components/DeploymentDetailsPage.jsx
@@ -1,5 +1,5 @@
 import { useLocation } from 'react-router';
-import { Container, Card, ListGroup } from 'react-bootstrap';
+import { Container, Table } from 'react-bootstrap';
 
 export default function DeploymentDetailsPage() {
     const location = useLocation();
@@ -11,61 +11,103 @@ export default function DeploymentDetailsPage() {
 
     return (
         <Container className="mt-4">
-            <Card>
-                <Card.Header>Deployment Details</Card.Header>
-                <Card.Body>
-                    <Card.Title>{item.date}</Card.Title>
-                    <Card.Text>
-                        <strong>Cloud Provider:</strong> {item.provider}<br />
-                        <strong>Created User:</strong> {item.created_usr}<br />
-                        <strong>Last Updated User:</strong> {item.last_updated_usr}<br />
-                    </Card.Text>
-                    <Card.Text>
-                        <strong>Lambda Functions:</strong>
-                        <ListGroup>
-                            {item.lambda.map((lambda, index) => (
-                                <ListGroup.Item key={index}>
-                                    <strong>App:</strong> {lambda.app}<br />
-                                    <strong>Owner:</strong> {lambda.owner}<br />
-                                    <strong>Additional Configs:</strong> {lambda.additional_configs}<br />
-                                    <strong>Function Name:</strong> {lambda.function_name}<br />
-                                    <strong>Scrum Team:</strong> {lambda.scrum_team}
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    </Card.Text>
-                    <Card.Text>
-                        <strong>Contact Flows:</strong>
-                        <ListGroup>
-                            {item.contact_flows.map((flow, index) => (
-                                <ListGroup.Item key={index}>
-                                    <strong>Name:</strong> {flow.name}<br />
-                                    <strong>Type:</strong> {flow.type}<br />
-                                    <strong>New:</strong> {flow.new}<br />
-                                    <strong>Bitbucket Link:</strong> {flow.bitbucket_link}
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    </Card.Text>
-                    <Card.Text>
-                        <strong>API Gateways:</strong>
-                        <ListGroup>
-                            {item.api_gateway.map((api, index) => (
-                                <ListGroup.Item key={index}>
-                                    <strong>API Gateway Name:</strong> {api.api_gateway_name}<br />
-                                    <strong>Route:</strong> {api.route}<br />
-                                    <strong>Method:</strong> {api.method}<br />
-                                    <strong>Authorization:</strong> {api.authorization}<br />
-                                    <strong>Lambda Function:</strong> {api.lambda_function}<br />
-                                    <strong>Owner:</strong> {api.owner}<br />
-                                    <strong>Scrum Team:</strong> {api.scrum_team}<br />
-                                    <strong>API Type:</strong> {api.api_type}
-                                </ListGroup.Item>
-                            ))}
-                        </ListGroup>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
+            <h2>Deployment Details</h2>
+            <Table striped bordered hover>
+                <tbody>
+                    <tr>
+                        <th>Date</th>
+                        <td>{item.date}</td>
+                    </tr>
+                    <tr>
+                        <th>Cloud Provider</th>
+                        <td>{item.provider}</td>
+                    </tr>
+                    <tr>
+                        <th>Created User</th>
+                        <td>{item.created_usr}</td>
+                    </tr>
+                    <tr>
+                        <th>Last Updated User</th>
+                        <td>{item.last_updated_usr}</td>
+                    </tr>
+                </tbody>
+            </Table>
+
+            <h3>Lambda Functions</h3>
+            <Table striped bordered hover>
+                <thead className="bg-primary text-white">
+                    <tr>
+                        <th>App</th>
+                        <th>Owner</th>
+                        <th>Additional Configs</th>
+                        <th>Function Name</th>
+                        <th>Scrum Team</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {item.lambda.map((lambda, index) => (
+                        <tr key={index}>
+                            <td>{lambda.app}</td>
+                            <td>{lambda.owner}</td>
+                            <td>{lambda.additional_configs}</td>
+                            <td>{lambda.function_name}</td>
+                            <td>{lambda.scrum_team}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+            <h3>Contact Flows</h3>
+            <Table striped bordered hover>
+                <thead className="bg-primary text-white">
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>New</th>
+                        <th>Bitbucket Link</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {item.contact_flows.map((flow, index) => (
+                        <tr key={index}>
+                            <td>{flow.name}</td>
+                            <td>{flow.type}</td>
+                            <td>{flow.new}</td>
+                            <td>{flow.bitbucket_link}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
+
+            <h3>API Gateways</h3>
+            <Table striped bordered hover>
+                <thead className="bg-primary text-white">
+                    <tr>
+                        <th>API Gateway Name</th>
+                        <th>Route</th>
+                        <th>Method</th>
+                        <th>Authorization</th>
+                        <th>Lambda Function</th>
+                        <th>Owner</th>
+                        <th>Scrum Team</th>
+                        <th>API Type</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {item.api_gateway.map((api, index) => (
+                        <tr key={index}>
+                            <td>{api.api_gateway_name}</td>
+                            <td>{api.route}</td>
+                            <td>{api.method}</td>
+                            <td>{api.authorization}</td>
+                            <td>{api.lambda_function}</td>
+                            <td>{api.owner}</td>
+                            <td>{api.scrum_team}</td>
+                            <td>{api.api_type}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </Table>
         </Container>
     );
 }
